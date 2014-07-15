ba04656f13c3ad8f90bc6143c5b4e643183ae53d
Jeremy Fox <foxjerem@gmail.com>
2014-07-15 09:36:20 +0100

    Test skeleton for grid

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,32 +4,32 @@ describe Game do
 
 	context 'At the start of the game' do
 	
-		it 'can make players' do
+		xit 'can make players' do
 		end
 		
-		it 'can make ships' do
+		xit 'can make ships' do
 		end
 
-		it 'can make grits' do
+		xit 'can make grits' do
 		end
 
-		it 'can give instruction for deployment' do 
+		xit 'can give instruction for deployment' do 
 		end
 
 	end
 
 	context 'In round n' do 
 
-		it 'can start the game' do
+		xit 'can start the game' do
 		end
 
-		it 'can set turns' do
+		xit 'can set turns' do
 		end
 
-		it 'can declare victory' do
+		xit 'can declare victory' do
 		end
 
-		it 'can end the game' do
+		xit 'can end the game' do
 		end
 
 	end
