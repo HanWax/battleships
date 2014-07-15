14042779e1e1ac3370e8c7a4e65e2a2d2caae116
Jeremy Fox <foxjerem@gmail.com>
2014-07-15 11:01:12 +0100

    Added basic grid functionality - size and cells

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,3 +1,24 @@
+require './lib/cell'
+
 class Grid
 
+	DEFAULT_SIZE = 10
+
+	attr_reader :size
+
+	def initialize(options = {})
+		@size = options.fetch(:size, DEFAULT_SIZE)
+		@cells = create_cells
+	end
+
+	def cells
+		@cells ||= []
+	end
+
+	def create_cells
+		(size**2).times { cells << Cell.new }
+		cells
+	end
+
 end
+
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,5 +1,25 @@
 require 'grid'
 
 describe Grid do
+
+	let(:grid) { Grid.new}
+
+	it 'should have a default size of 10' do
+		expect(grid.size).to eq Grid::DEFAULT_SIZE
+	end
+
+	it 'can be initialized with a size of N' do
+		expect(Grid.new(size: 12).size).to eq 12
+	end
+
+	it 'can hold cells' do
+		expect(grid.cells.is_a?(Array)).to be true
+	end
+
+	it 'holds size x size number of cells on initialization' do
+		expect(grid.cells.count).to eq grid.size**2
+	end
 	
+
+
 end
