6b4344cf37e1836ef76bad9962d3dff9b12c1be7
Jeremy Fox <foxjerem@gmail.com>
2014-07-17 11:25:12 +0100

    Updated the game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,21 @@
 require './lib/player'
 require './lib/ship'
+
 class Game
 
-	attr_accessor :players
+	attr_accessor :current_player, :other_player
 
 	def initialize
-			@players = player_factory
+			@current_player = player_factory
+			@other_player = player_factory
+	end
+
+	def players
+		[@current_player, @other_player]
 	end
 
 	def player_factory
-		[Player.new, Player.new] 
+		Player.new
 	end
 
 	def ship_factory
@@ -21,9 +27,28 @@ class Game
 		]
 	end
 
+	def start_game
+		players[0].deploy_ships(ship_factory)
+		players[1].deploy_ships(ship_factory)
+		play_game
+	end
+
+	def change_turn
+		@current_player, @other_player = @other_player, @current_player
+	end
+
+	def play_game
+
+	end
+
 	def declare_victory
 		return "player2 wins!" if players[0].grid.count_sunken_ships == 5
 		return "player1 wins!" if players[1].grid.count_sunken_ships == 5
+	end
+
+	def end_game
+		puts "----GAME OVER----"
+		exit
 	end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,25 @@ require 'game'
 
 describe Game do
 
-	let(:game) { Game.new}
+	let(:game) 		{ Game.new																							}
+	let(:player1) { double :player1, :grid => grid1, :deploy_ships => nil	}
+	let(:player2) { double :player2, :grid => grid2, :deploy_ships => nil	}
+	let(:grid1)  	{ double :grid1, :count_sunken_ships => 5 							}
+	let(:grid2)		{ double :grid2, :count_sunken_ships => 0 							}
 
 	context 'At the start of the game' do
 	
 		it 'should start with 2 instances of the player class' do
 			expect(game.players[0]).to be_an_instance_of Player
 			expect(game.players[1]).to be_an_instance_of Player
+		end
+
+		it 'should know which is the current player' do
+			expect(game.current_player).to be game.players[0]
+		end
+
+		it 'should know which is the other player' do
+			expect(game.other_player).to be game.players[1]
 		end
 		
 		it 'can make an array of ships' do
@@ -23,29 +35,33 @@ describe Game do
 			expect(game.ship_factory[4]).to be_an_instance_of PatrolBoat
 		end
 
-		xit 'can give instruction for deployment' do 
+		it 'can start the game' do
+			game.current_player = player1
+			game.other_player = player2
+			expect(player1).to receive(:deploy_ships)
+			expect(player2).to receive(:deploy_ships)
+			game.start_game
 		end
 
 	end
 
 	context 'In round n' do 
 
-		xit 'can start the game' do
-		end
-
-		xit 'can set turns' do
+		it 'can change turns' do
+			player1 = game.current_player
+			game.change_turn
+			expect(game.other_player).to be player1
 		end
 
 		it 'can declare victory' do
-			grid1 = double :grid1, :count_sunken_ships => 5
-			grid2 = double :grid2, :count_sunken_ships => 0
-			player1 = double :player1, :grid => grid1
-			player2 = double :player2, :grid => grid2
-			game.players = [player1, player2]
+			game.current_player = player1
+			game.other_player = player2
 			expect(game.declare_victory).to eq "player2 wins!"
 		end
 
-		xit 'can end the game' do
+		it 'can end the game' do
+			expect(STDOUT).to receive(:puts).with("----GAME OVER----")
+			expect { game.end_game }.to raise_exception(SystemExit)
 		end
 
 	end
