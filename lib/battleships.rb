0cf8c040414f00977a56af07926ecd6714404f2a
cmew3 <c.wheeler-booth@cantab.net>
2014-07-21 16:29:26 +0100

    added first scenario - registering

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -2,8 +2,12 @@ require 'sinatra/base'
 
 class Battleships < Sinatra::Base
   get '/' do
-    erb :index
+    erb :index			
   end
+
+	get "/newgame.erb" do
+  		erb :newgame
+	end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
